88237f71915fe829030b266fcf3f1bbf203249ba
mathieu ferry <mathieu.ferry87@gmail.com>
2022-08-29 18:05:25 +0200

    creation des models

---
+++ app/models/project.rb
@@ -0,0 +1,4 @@
+class Project < ApplicationRecord
+  belongs_to :offer
+  belongs_to :freelancer
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,86 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_08_29_134157) do
+ActiveRecord::Schema[7.0].define(version: 2022_08_29_160333) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "chatrooms", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "elements", force: :cascade do |t|
+    t.string "title"
+    t.text "description"
+    t.integer "price"
+    t.bigint "quote_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["quote_id"], name: "index_elements_on_quote_id"
+  end
+
+  create_table "freelancers", force: :cascade do |t|
+    t.string "first_name"
+    t.string "last_name"
+    t.string "email"
+    t.string "phone_number"
+    t.string "address"
+    t.string "siret"
+    t.text "description"
+    t.integer "number_of_projects"
+    t.date "batch_date"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_freelancers_on_user_id"
+  end
+
+  create_table "messages", force: :cascade do |t|
+    t.text "content"
+    t.bigint "chatroom_id", null: false
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["chatroom_id"], name: "index_messages_on_chatroom_id"
+    t.index ["user_id"], name: "index_messages_on_user_id"
+  end
+
+  create_table "offers", force: :cascade do |t|
+    t.string "title"
+    t.text "description"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_offers_on_user_id"
+  end
+
+  create_table "projects", force: :cascade do |t|
+    t.boolean "status"
+    t.bigint "offer_id", null: false
+    t.bigint "freelancer_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["freelancer_id"], name: "index_projects_on_freelancer_id"
+    t.index ["offer_id"], name: "index_projects_on_offer_id"
+  end
+
+  create_table "quotes", force: :cascade do |t|
+    t.integer "total_price"
+    t.bigint "freelancer_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["freelancer_id"], name: "index_quotes_on_freelancer_id"
+  end
+
+  create_table "skills", force: :cascade do |t|
+    t.string "title"
+    t.integer "rating"
+    t.bigint "freelancer_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["freelancer_id"], name: "index_skills_on_freelancer_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -26,4 +103,13 @@ ActiveRecord::Schema[7.0].define(version: 2022_08_29_134157) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "elements", "quotes"
+  add_foreign_key "freelancers", "users"
+  add_foreign_key "messages", "chatrooms"
+  add_foreign_key "messages", "users"
+  add_foreign_key "offers", "users"
+  add_foreign_key "projects", "freelancers"
+  add_foreign_key "projects", "offers"
+  add_foreign_key "quotes", "freelancers"
+  add_foreign_key "skills", "freelancers"
 end
