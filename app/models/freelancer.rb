eb60e216085ca9ac478ab1402cb466dfb6997f80
mathieu ferry <mathieu.ferry87@gmail.com>
2022-08-30 16:21:48 +0200

    seeds

--- app/models/freelancer.rb
+++ app/models/freelancer.rb
@@ -3,6 +3,7 @@ class Freelancer < ApplicationRecord
   has_many :skills, dependent: :destroy
   has_many :quotes
   has_many :collaborations
+  has_one_attached :photo
   validates :user_id, uniqueness: true
   validates :first_name, :last_name, :phone_number, :address, :siret, :description, :email, presence: true
   validates :number_of_projects, :batch_date, presence: true
