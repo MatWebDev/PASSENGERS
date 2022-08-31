9cd28e2317f3d850781f2bd5b9a7cb79d877c3b0
mathieu ferry <mathieu.ferry87@gmail.com>
2022-08-31 17:00:19 +0200

    methodes de collaborations

--- app/controllers/collaborations_controller.rb
+++ app/controllers/collaborations_controller.rb
@@ -1,18 +1,28 @@
 class CollaborationsController < ApplicationController
   def create
     @user = current_user
-    @projet = Project.find(params[:project_id])
-    
-    @collaboration = Collaboration.new(collaboration_params)
+    @collaboration = Collaboration.new(params_collaboration)
+    @project = @collaboration.project
     if @collaboration.save
       redirect_to dashboard_path
     else
-      render quote_path(quote)
+      render project_path(@project), status: :unprocessable_entity
     end
+  end
 
+  def update
+    @collaboration = Collaboration.find(params[:id])
+    case params[:status]
+    when 'accepted'
+      @collaboration.update(status: 'accepted')
+    when 'declined'
+      @collaboration.update(status: 'declined')
+    end
   end
 
   private
 
-
+  def params_collaboration
+    params.require(:collaboration).permit(:status, :title, :total_price)
+  end
 end
