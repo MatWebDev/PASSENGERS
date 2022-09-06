440d6dbe58a3c836fe4d50b5bbad5047851c9fe0
mathieu ferry <mathieu.ferry87@gmail.com>
2022-09-06 14:42:55 +0200

    front de la banner du project form

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -11,25 +11,4 @@ class PagesController < ApplicationController
     @collaborations = Collaboration.where(user_id: current_user.id)
     @projects = Project.where(user_id: current_user.id)
   end
-
-  # def selectionfreelancers
-  #   @project = current_user.project
-  #   freelancers = User.where(role: 'freelancer')
-  #   @junior_freelancers = []
-  #   @intermediate_freelancers = []
-  #   @senior_freelancers = []
-  #   @junior_intermediate_freelancers = []
-
-  #   freelancers.each do |freelancer|
-  #     if freelancer.score_exp <= 33
-  #       @junior_freelancers << freelancer
-  #       @junior_intermediate_freelancers << freelancer
-  #     elsif freelancer.score_exp > 33 && freelancer.score_exp <= 66
-  #       @intermediate_freelancers << freelancer
-  #       @junior_intermediate_freelancers << freelancer
-  #     elsif freelancer.score_exp > 66
-  #       @senior_freelancers << freelancer
-  #     end
-  #   end
-  # end
 end
