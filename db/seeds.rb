c1842fef47e4795dadb790eb05d27418152a5b7c
Guillaume Teillais <guillaume.teillais@gmail.com>
2022-09-07 18:10:57 +0200

    seed freelancers

--- db/seeds.rb
+++ db/seeds.rb
@@ -19,7 +19,7 @@ user = User.new(
   description: "I'm used to working with Ruby on Rails for the back, and ReactJs or html5/css3/Javascript for the front depending on the needs. I also like to discover new technologies or libraries when the project allows it!
   I can work on a site from A to Z as well as adapt to an ongoing project, web or mobile.
   Do not hesitate to look at my various professional experiences to know all the technologies on which I have been able to work and to contact me for further information.",
-  skills: ['ruby', 'python', 'javascript', 'java', 'html5', 'css3', 'react', 'rails', 'wordpress', 'bootstrap'],
+  skills: ['ruby', 'rails', 'python', 'javascript', 'java', 'html5', 'css3', 'react', 'wordpress', 'bootstrap'],
   # score_exp: 85
 )
 user.photo.attach(io: file, filename: 'marilou.jpeg', content_type: 'image/jpeg')
@@ -40,32 +40,10 @@ user = User.new(
   description: "Having already realized many applications for start-ups, I am specialized in the realization of MVP (Minimum Viable Product).
   I develop the entire application, front-end and back-end, and deliver the turnkey application.
   I also accompany for the publication on the blinds.",
-  skills: ['ruby', 'python', 'javascript', 'java', 'html5'],
+  skills: ['ruby', 'rails', 'react', 'javascript', 'html5', 'css3'],
   # score_exp: 35
 )
 user.photo.attach(io: file, filename: 'guillaume.jpg', content_type: 'image/jpeg')
-user.save!
-
-file = URI.open('https://avatars.githubusercontent.com/u/102875112?v=4')
-user = User.new(
-  role: 'freelancer',
-  first_name: 'Jerome',
-  last_name: 'Pomonti',
-  email: 'jerome@test.com',
-  password: '123456',
-  phone_number: '0612345673',
-  address: 'Noumea',
-  siret: '123 789 456 00018',
-  number_of_projects: 5,
-  year_exp: 1,
-  description: "Freshly graduated from Epitech, I'm a bit of a jack-of-all-trades.
-  I have a strong appetite for functional projects even if I have a lot of experience in technical projects.
-  I can work on web development projects (fullstack: Backend in Python and Frontend in javascript) as well as on IS architecture in AGILE method. I like working independently and I adapt very easily to a new work group.
-  I'm more successful on python in back-end web development.",
-  skills: ['ruby', 'python', 'wordpress', 'html5'],
-  # score_exp: 19
-)
-user.photo.attach(io: file, filename: 'jerome.jpg', content_type: 'image/jpeg')
 user.save!
 
 file = URI.open('https://avatars.githubusercontent.com/u/105813372?v=4')
@@ -89,16 +67,16 @@ user = User.new(
 user.photo.attach(io: file, filename: 'mathieu.jpg', content_type: 'image/jpeg')
 user.save!
 
-file = URI.open('https://avatars.githubusercontent.com/u/105067544?v=4')
+file = URI.open('https://avatars.githubusercontent.com/u/91216872?v=4')
 user = User.new(
   role: 'freelancer',
   first_name: 'Maewenn',
   last_name: 'Drean',
   email: 'md@test.com',
   password: '123456',
-  phone_number: '0612345671',
-  address: 'Martigues',
-  siret: '123 456 789 00018',
+  phone_number: '0612375671',
+  address: 'Nice',
+  siret: '123 456 719 00018',
   number_of_projects: 25,
   year_exp: 5,
   description: "I'm used to working with Ruby on Rails for the back, and ReactJs or html5/css3/Javascript for the front depending on the needs. I also like to discover new technologies or libraries when the project allows it!
@@ -107,63 +85,41 @@ user = User.new(
   skills: ['ruby', 'python', 'javascript', 'java', 'html5', 'css3', 'react', 'rails', 'wordpress', 'bootstrap'],
   # score_exp: 85
 )
-user.photo.attach(io: file, filename: 'marilou.jpg', content_type: 'image/jpeg')
+user.photo.attach(io: file, filename: 'maewenn.jpg', content_type: 'image/jpeg')
 user.save!
 
-file = URI.open('https://avatars.githubusercontent.com/u/103571112?v=4')
+file = URI.open('https://avatars.githubusercontent.com/u/95645345?v=4')
 user = User.new(
   role: 'freelancer',
   first_name: 'Jean-Guillaume',
-  last_name: 'Yangui',
+  last_name: 'Vicente',
   email: 'jg@test.com',
   password: '123456',
-  phone_number: '0612345672',
-  address: 'Aix-en-Provence',
-  siret: '456 789 123 00018',
+  phone_number: '0612335672',
+  address: 'Marseille',
+  siret: '456 789 123 00015',
   number_of_projects: 10,
   year_exp: 2,
   description: "Having already realized many applications for start-ups, I am specialized in the realization of MVP (Minimum Viable Product).
   I develop the entire application, front-end and back-end, and deliver the turnkey application.
   I also accompany for the publication on the blinds.",
-  skills: ['ruby', 'python', 'javascript', 'java', 'html5'],
+  skills: ['ruby', 'python', 'javascript', 'rails', 'html5'],
   # score_exp: 35
 )
-user.photo.attach(io: file, filename: 'guillaume.jpg', content_type: 'image/jpeg')
+user.photo.attach(io: file, filename: 'jean-guillaume.jpg', content_type: 'image/jpeg')
 user.save!
 
-file = URI.open('https://avatars.githubusercontent.com/u/102875112?v=4')
-user = User.new(
-  role: 'freelancer',
-  first_name: 'Loris',
-  last_name: 'DeNice',
-  email: 'ln@test.com',
-  password: '123456',
-  phone_number: '0612345673',
-  address: 'Noumea',
-  siret: '123 789 456 00018',
-  number_of_projects: 5,
-  year_exp: 1,
-  description: "Freshly graduated from Epitech, I'm a bit of a jack-of-all-trades.
-  I have a strong appetite for functional projects even if I have a lot of experience in technical projects.
-  I can work on web development projects (fullstack: Backend in Python and Frontend in javascript) as well as on IS architecture in AGILE method. I like working independently and I adapt very easily to a new work group.
-  I'm more successful on python in back-end web development.",
-  skills: ['ruby', 'python', 'wordpress', 'html5'],
-  # score_exp: 19
-)
-user.photo.attach(io: file, filename: 'jerome.jpg', content_type: 'image/jpeg')
-user.save!
-
-file = URI.open('https://avatars.githubusercontent.com/u/105813372?v=4')
+file = URI.open('https://avatars.githubusercontent.com/u/2136880?v=4')
 user = User.new(
   role: 'freelancer',
   first_name: 'Damien',
   last_name: 'Vincent',
   email: 'dv@test.com',
   password: '123456',
-  phone_number: '0612345674',
-  address: 'Bourreac',
-  siret: '789 123 456 00018',
-  number_of_projects: 90,
+  phone_number: '0622345674',
+  address: 'Marseille',
+  siret: '789 123 125 00018',
+  number_of_projects: 200,
   year_exp: 10,
   description: "Engineer for 6 years, I specialize in Java/JEE fullstack web development.
   During these five years, I had the opportunity to provide solutions to complex problems all at high availability and high performance.
@@ -171,83 +127,42 @@ user = User.new(
   skills: ['ruby', 'python', 'javascript', 'java', 'html5', 'css3', 'react', 'rails', 'wordpress', 'bootstrap', 'photoshop', 'illustrator', 'woocommerce', 'php', 'csharp'],
   # score_exp: 245
 )
-user.photo.attach(io: file, filename: 'mathieu.jpg', content_type: 'image/jpeg')
+user.photo.attach(io: file, filename: 'damien.jpg', content_type: 'image/jpeg')
 user.save!
 
-file = URI.open('https://avatars.githubusercontent.com/u/105067544?v=4')
-user = User.new(
-  role: 'freelancer',
-  first_name: 'Christophe',
-  last_name: 'LeBoss',
-  email: 'cb@test.com',
-  password: '123456',
-  phone_number: '0612345671',
-  address: 'Martigues',
-  siret: '123 456 789 00018',
-  number_of_projects: 25,
-  year_exp: 5,
-  description: "I'm used to working with Ruby on Rails for the back, and ReactJs or html5/css3/Javascript for the front depending on the needs. I also like to discover new technologies or libraries when the project allows it!
-  I can work on a site from A to Z as well as adapt to an ongoing project, web or mobile.
-  Do not hesitate to look at my various professional experiences to know all the technologies on which I have been able to work and to contact me for further information.",
-  skills: ['ruby', 'python', 'javascript', 'java', 'html5', 'css3', 'react', 'rails', 'wordpress', 'bootstrap'],
-  # score_exp: 85
-)
-user.photo.attach(io: file, filename: 'marilou.jpg', content_type: 'image/jpeg')
-user.save!
 
-file = URI.open('https://avatars.githubusercontent.com/u/103571112?v=4')
-user = User.new(
-  role: 'freelancer',
-  first_name: 'Serge',
-  last_name: 'Java',
-  email: 'sj@test.com',
-  password: '123456',
-  phone_number: '0612345672',
-  address: 'Aix-en-Provence',
-  siret: '456 789 123 00018',
-  number_of_projects: 10,
-  year_exp: 2,
-  description: "Having already realized many applications for start-ups, I am specialized in the realization of MVP (Minimum Viable Product).
-  I develop the entire application, front-end and back-end, and deliver the turnkey application.
-  I also accompany for the publication on the blinds.",
-  skills: ['ruby', 'python', 'javascript', 'java', 'html5'],
-  # score_exp: 35
-)
-user.photo.attach(io: file, filename: 'guillaume.jpg', content_type: 'image/jpeg')
-user.save!
-
-file = URI.open('https://avatars.githubusercontent.com/u/102875112?v=4')
+file = URI.open('https://avatars.githubusercontent.com/u/59578436?v=4')
 user = User.new(
   role: 'freelancer',
   first_name: 'Théo',
-  last_name: 'LeMonstre',
+  last_name: 'Bondaz dit "le monstre"',
   email: 'tm@test.com',
   password: '123456',
-  phone_number: '0612345673',
-  address: 'Noumea',
-  siret: '123 789 456 00018',
+  phone_number: '0712345673',
+  address: 'Marseille',
+  siret: '123 759 456 00018',
   number_of_projects: 5,
   year_exp: 1,
-  description: "Freshly graduated from Epitech, I'm a bit of a jack-of-all-trades.
+  description: "Freelancer and Le Wagon teacher. I'm a bit of a jack-of-all-trades.
   I have a strong appetite for functional projects even if I have a lot of experience in technical projects.
   I can work on web development projects (fullstack: Backend in Python and Frontend in javascript) as well as on IS architecture in AGILE method. I like working independently and I adapt very easily to a new work group.
   I'm more successful on python in back-end web development.",
-  skills: ['ruby', 'python', 'wordpress', 'html5'],
+  skills: ['ruby', 'rails','javascript', 'css3', 'html5'],
   # score_exp: 19
 )
-user.photo.attach(io: file, filename: 'jerome.jpg', content_type: 'image/jpeg')
+user.photo.attach(io: file, filename: 'theo.jpg', content_type: 'image/jpeg')
 user.save!
 
-file = URI.open('https://avatars.githubusercontent.com/u/105813372?v=4')
+file = URI.open('https://avatars.githubusercontent.com/u/63111765?v=4')
 user = User.new(
   role: 'freelancer',
   first_name: 'Maélie',
-  last_name: 'DeNantes',
-  email: 'mn@test.com',
+  last_name: 'Poussier',
+  email: 'maep@test.com',
   password: '123456',
-  phone_number: '0612345674',
-  address: 'Bourreac',
-  siret: '789 123 456 00018',
+  phone_number: '0612345974',
+  address: 'Nantes',
+  siret: '789 123 476 00018',
   number_of_projects: 90,
   year_exp: 10,
   description: "Engineer for 6 years, I specialize in Java/JEE fullstack web development.
@@ -256,19 +171,19 @@ user = User.new(
   skills: ['ruby', 'python', 'javascript', 'java', 'html5', 'css3', 'react', 'rails', 'wordpress', 'bootstrap', 'photoshop', 'illustrator', 'woocommerce', 'php', 'csharp'],
   # score_exp: 245
 )
-user.photo.attach(io: file, filename: 'mathieu.jpg', content_type: 'image/jpeg')
+user.photo.attach(io: file, filename: 'maelie.jpg', content_type: 'image/jpeg')
 user.save!
 
-file = URI.open('https://avatars.githubusercontent.com/u/105067544?v=4')
+file = URI.open('https://avatars.githubusercontent.com/u/71704300?v=4')
 user = User.new(
   role: 'freelancer',
-  first_name: 'Jean-Loup',
-  last_name: 'LeCrack',
-  email: 'jlc@test.com',
+  first_name: 'John-Wolf',
+  last_name: 'Peres',
+  email: 'jlp@test.com',
   password: '123456',
-  phone_number: '0612345671',
-  address: 'Martigues',
-  siret: '123 456 789 00018',
+  phone_number: '0615345671',
+  address: 'Toulon',
+  siret: '123 456 749 00018',
   number_of_projects: 25,
   year_exp: 5,
   description: "I'm used to working with Ruby on Rails for the back, and ReactJs or html5/css3/Javascript for the front depending on the needs. I also like to discover new technologies or libraries when the project allows it!
@@ -277,114 +192,71 @@ user = User.new(
   skills: ['ruby', 'python', 'javascript', 'java', 'html5', 'css3', 'react', 'rails', 'wordpress', 'bootstrap'],
   # score_exp: 85
 )
-user.photo.attach(io: file, filename: 'marilou.jpg', content_type: 'image/jpeg')
+user.photo.attach(io: file, filename: 'jean-loup.jpg', content_type: 'image/jpeg')
 user.save!
 
-file = URI.open('https://avatars.githubusercontent.com/u/103571112?v=4')
-user = User.new(
-  role: 'freelancer',
-  first_name: 'John',
-  last_name: 'Wolf',
-  email: 'jw@test.com',
-  password: '123456',
-  phone_number: '0612345672',
-  address: 'Aix-en-Provence',
-  siret: '456 789 123 00018',
-  number_of_projects: 10,
-  year_exp: 2,
-  description: "Having already realized many applications for start-ups, I am specialized in the realization of MVP (Minimum Viable Product).
-  I develop the entire application, front-end and back-end, and deliver the turnkey application.
-  I also accompany for the publication on the blinds.",
-  skills: ['ruby', 'python', 'javascript', 'java', 'html5'],
-  # score_exp: 35
-)
-user.photo.attach(io: file, filename: 'guillaume.jpg', content_type: 'image/jpeg')
-user.save!
-
-file = URI.open('https://avatars.githubusercontent.com/u/102875112?v=4')
+file = URI.open('https://avatars.githubusercontent.com/u/2471555?v=4')
 user = User.new(
   role: 'freelancer',
   first_name: 'Boris',
   last_name: 'Paillard',
   email: 'bp@test.com',
   password: '123456',
-  phone_number: '0612345673',
-  address: 'Noumea',
-  siret: '123 789 456 00018',
-  number_of_projects: 5,
-  year_exp: 1,
-  description: "Freshly graduated from Epitech, I'm a bit of a jack-of-all-trades.
+  phone_number: '0612345573',
+  address: 'Paris',
+  siret: '123 789 456 70018',
+  number_of_projects: 95,
+  year_exp: 7,
+  description: "CEO Le Wagon & freshly graduated from Epitech, I'm a bit of a jack-of-all-trades.
   I have a strong appetite for functional projects even if I have a lot of experience in technical projects.
   I can work on web development projects (fullstack: Backend in Python and Frontend in javascript) as well as on IS architecture in AGILE method. I like working independently and I adapt very easily to a new work group.
   I'm more successful on python in back-end web development.",
-  skills: ['ruby', 'python', 'wordpress', 'html5'],
+  skills: ['ruby', 'python', 'javascript', 'java', 'html5', 'css3', 'react', 'rails', 'wordpress', 'bootstrap'],
   # score_exp: 19
 )
-user.photo.attach(io: file, filename: 'jerome.jpg', content_type: 'image/jpeg')
+user.photo.attach(io: file, filename: 'boris.jpg', content_type: 'image/jpeg')
 user.save!
 
-file = URI.open('https://avatars.githubusercontent.com/u/105813372?v=4')
+file = URI.open('https://avatars.githubusercontent.com/u/414418?v=4')
 user = User.new(
   role: 'freelancer',
   first_name: 'Sébastien',
   last_name: 'Saunier',
   email: 'ss@test.com',
   password: '123456',
-  phone_number: '0612345674',
-  address: 'Bourreac',
-  siret: '789 123 456 00018',
-  number_of_projects: 90,
+  phone_number: '0614345674',
+  address: 'Paris',
+  siret: '789 123 456 00518',
+  number_of_projects: 490,
   year_exp: 10,
-  description: "Engineer for 6 years, I specialize in Java/JEE fullstack web development.
+  description: "CTO & Co-Founder @lewagon. Engineer for 6 years, I specialize in Java/JEE fullstack web development.
   During these five years, I had the opportunity to provide solutions to complex problems all at high availability and high performance.
   Data and exchange security has always been one of my top priorities (PCI-DSS standard).",
   skills: ['ruby', 'python', 'javascript', 'java', 'html5', 'css3', 'react', 'rails', 'wordpress', 'bootstrap', 'photoshop', 'illustrator', 'woocommerce', 'php', 'csharp'],
   # score_exp: 245
 )
-user.photo.attach(io: file, filename: 'mathieu.jpg', content_type: 'image/jpeg')
-user.save!
-
-file = URI.open('https://avatars.githubusercontent.com/u/102875112?v=4')
-user = User.new(
-  role: 'freelancer',
-  first_name: 'Dylane',
-  last_name: 'DeMarseille',
-  email: 'dmrs@test.com',
-  password: '123456',
-  phone_number: '0612345673',
-  address: 'Noumea',
-  siret: '123 789 456 00018',
-  number_of_projects: 5,
-  year_exp: 1,
-  description: "Freshly graduated from Epitech, I'm a bit of a jack-of-all-trades.
-  I have a strong appetite for functional projects even if I have a lot of experience in technical projects.
-  I can work on web development projects (fullstack: Backend in Python and Frontend in javascript) as well as on IS architecture in AGILE method. I like working independently and I adapt very easily to a new work group.
-  I'm more successful on python in back-end web development.",
-  skills: ['ruby', 'python', 'wordpress', 'html5'],
-  # score_exp: 19
-)
-user.photo.attach(io: file, filename: 'mathieu.jpg', content_type: 'image/jpeg')
+user.photo.attach(io: file, filename: 'sebastien.jpg', content_type: 'image/jpeg')
 user.save!
 
 User.create!(
   role: 'customer',
-  first_name: 'Jean-Michel',
-  last_name: 'Test',
-  email: 'jm@test.com',
+  first_name: 'Jérome',
+  last_name: 'Pomonti',
+  email: 'jerome@test.com',
   password: '123456',
   phone_number: '0612345685',
-  address: 'Marseille',
-  company_name: 'Jean-Michel&Fils'
+  address: 'Nouméa',
+  company_name: 'API Culteur'
 )
 
 puts 'Users created successfully man!'
 
 Project.create!(
-  title: "e-shop creation for JM company",
+  title: "e-Shop creation",
   description: "I need an e-shop for my company, I don't know anything about web development, please help me!",
   origin: "Creation",
   website: "E-shop under 10 items",
-  user_id: User.find_by(first_name: 'Jean-Michel').id,
+  user_id: User.find_by(first_name: 'Jérome').id,
   score_difficulty: 2
 )
 
@@ -393,7 +265,7 @@ Project.create!(
   description: "I need a one page website for my company, I don't know anything about web development, please help me!",
   origin: "Creation",
   website: "One Page",
-  user_id: User.find_by(first_name: 'Jean-Michel').id,
+  user_id: User.find_by(first_name: 'Jérome').id,
   score_difficulty: 1
 )
 
@@ -402,7 +274,7 @@ Project.create!(
   description: "We need a big eshop to sale different products in several categories. We don't have development department so we need a freelancer!",
   origin: "Creation",
   website: "E-shop over 10 items",
-  user_id: User.find_by(first_name: 'Jean-Michel').id,
+  user_id: User.find_by(first_name: 'Jérome').id,
   score_difficulty: 3
 )
 
@@ -411,16 +283,16 @@ Project.create!(
   description: "We need a web app to serve automatically a beer to our customer.",
   origin: "Creation",
   website: "WebApp",
-  user_id: User.find_by(first_name: 'Jean-Michel').id,
+  user_id: User.find_by(first_name: 'Jérome').id,
   score_difficulty: 4
 )
 
 Project.create!(
-  title: "Webapp JM&Fils",
+  title: "Webapp API Culteur",
   description: "We need a web app but we don't know why.",
   origin: "Creation",
   website: "WebApp",
-  user_id: User.find_by(first_name: 'Jean-Michel').id,
+  user_id: User.find_by(first_name: 'Jérome').id,
   score_difficulty: 4
 )
 
